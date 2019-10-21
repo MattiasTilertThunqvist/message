9e361e70e1db49f5596d47c3a1dee5a9466fef2e
MattiasTilertThunqvist <mattias@tilert.com>
2019-10-21 13:26:33 +0100

    Project setup incl Firebase

---
+++ app/src/main/java/com/tilert/message/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.tilert.message
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+
+        }
+
+
+        back_to_register_textview.setOnClickListener {
+            finish()
+        }
+
+    }
+}
