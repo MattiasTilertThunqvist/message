1dc6af60a9a8147c14c1c672e49f5d52201b04aa
MattiasTilertThunqvist <mattias@tilert.com>
2019-10-28 17:31:47 +0000

    - Setup recyclerviews and dependencies

--- app/src/main/java/com/tilert/message/ChatOverviewActivity.kt
+++ app/src/main/java/com/tilert/message/ChatOverviewActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_chat_overview.*
 
 class ChatOverviewActivity: AppCompatActivity() {
@@ -12,12 +15,39 @@ class ChatOverviewActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_overview)
-
+        setup()
 
         new_chat_button_chatoverview.setOnClickListener {
             val intent = Intent(this, NewChatActivity::class.java)
             startActivity(intent)
-        }
 
+        }
+    }
+
+    private fun setup() {
+        supportActionBar?.title = "Chats"
+
+        val adapter = GroupAdapter<GroupieViewHolder>()
+        recyclerview_chat_overview.adapter = adapter
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+    }
+}
+
+class ChatItem: Item<GroupieViewHolder>() {
+
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_chat_overview
     }
 }
--- app/src/main/java/com/tilert/message/NewChatActivity.kt
+++ app/src/main/java/com/tilert/message/NewChatActivity.kt
@@ -3,11 +3,43 @@ package com.tilert.message
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_new_chat.*
 
 class NewChatActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_chat)
+        setup()
+    }
+
+    private fun setup() {
+        supportActionBar?.title = "New chat"
+
+        val adapter = GroupAdapter<GroupieViewHolder>()
+        recyclerview_new_chat.adapter = adapter
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+    }
+}
+
+class UserItem: Item<GroupieViewHolder>() {
+
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_chat
     }
 }
