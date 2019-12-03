76ae71f9b0d11ca9813a80978679015119bde1e9
Mattias Tilert Thunqvist <mattias@tilert.com>
2019-12-03 11:19:05 +0000

    - Set up logic for sending messages

--- app/src/main/java/com/tilert/message/messages/ChatActivity.kt
+++ app/src/main/java/com/tilert/message/messages/ChatActivity.kt
@@ -1,50 +1,91 @@
 package com.tilert.message.messages
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.tilert.message.models.User
 import com.tilert.message.R
+import com.tilert.message.models.ChatMessage
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.android.synthetic.main.chat_row_from.view.*
+import kotlinx.android.synthetic.main.chat_row_to.view.*
 
 class ChatActivity: AppCompatActivity() {
+
+    val adapter = GroupAdapter<GroupieViewHolder>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
         setup()
+        listenForMessages()
     }
 
     private fun setup() {
         val user = intent.getParcelableExtra<User>(NewChatActivity.USER_KEY)
         supportActionBar?.title = user.username
 
-        val adapter = GroupAdapter<GroupieViewHolder>()
         recyclerview_chat_log.adapter = adapter
 
-        val chatItemFrom = ChatItemFrom()
-        val chatItemTo = ChatItemTo()
+        button_send_chat_log.setOnClickListener {
+            handleSendMessage()
+        }
+    }
 
-        adapter.add(chatItemFrom)
-        adapter.add(chatItemTo)
-        adapter.add(chatItemFrom)
-        adapter.add(chatItemTo)
-        adapter.add(chatItemFrom)
-        adapter.add(chatItemFrom)
-        adapter.add(chatItemTo)
-        adapter.add(chatItemFrom)
-        adapter.add(chatItemTo)
-        adapter.add(chatItemFrom)
+    private fun listenForMessages() {
+         val reference = FirebaseFirestore.getInstance().collection("/messages")
 
+        reference.addSnapshotListener { snapshot, exception ->
+            exception?.let { exception ->
+                Log.d("ChatActivity", exception.localizedMessage)
+                return@addSnapshotListener
+            }
+
+            val snapshot = snapshot.let { it } ?: return@addSnapshotListener
+
+            snapshot.forEach {
+                val chatMessage = it.toObject(ChatMessage::class.java)
+
+                if (chatMessage.fromId == FirebaseAuth.getInstance().uid) {
+                    adapter.add(ChatItemFrom(chatMessage.text))
+                } else {
+                    adapter.add(ChatItemTo(chatMessage.text))
+                }
+            }
+
+        }
+    }
+
+    private fun handleSendMessage() {
+        val reference = FirebaseFirestore.getInstance().collection("/messages").document()
+
+        val id = reference.id
+        val text = edittext_chat_log.text.toString()
+        val fromId = FirebaseAuth.getInstance().uid.let { it } ?: return
+        val toId = intent.getParcelableExtra<User>(NewChatActivity.USER_KEY).uid
+        val timestamp = System.currentTimeMillis() / 1000 // Convert to seconds
+
+        val chatMessage = ChatMessage(id, text, fromId, toId, timestamp)
+
+        reference.set(chatMessage)
+            .addOnSuccessListener {
+
+            }
+            .addOnFailureListener {
+                // Handle error
+            }
     }
 }
 
-class ChatItemFrom: Item<GroupieViewHolder>() {
+class ChatItemFrom(val text: String ): Item<GroupieViewHolder>() {
 
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-
+        viewHolder.itemView.textView_chat_row_from.text = text
     }
 
     override fun getLayout(): Int {
@@ -52,10 +93,10 @@ class ChatItemFrom: Item<GroupieViewHolder>() {
     }
 }
 
-class ChatItemTo: Item<GroupieViewHolder>() {
+class ChatItemTo(val text: String): Item<GroupieViewHolder>() {
 
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-
+        viewHolder.itemView.textView_chat_row_to.text = text
     }
 
     override fun getLayout(): Int {
